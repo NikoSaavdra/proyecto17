1c85dab079303ba54b0c44f33b94d82c1cc71804
nicolas <nico@gmail.com>
2024-12-04 20:31:47 +0100

    Usando Jquery en la pagina

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,17 @@
+// Importar jQuery utilizando la sintaxis de módulos ES6
+import $ from 'jquery';
+
+// Esperamos que el DOM esté listo
+$(document).ready(function() {
+  // Asignar la acción de clic al botón
+  $('#boton').click(function() {
+    alert("¡Haz hecho clic en el botón!");
+  });
+});
+
+
+document.getElementById('boton').onclick = function() {
+    alert('Me ha presionado... me voy de vacaciones a relajarme');
+}
+
 console.log('Hola mundo... tercera vez');
