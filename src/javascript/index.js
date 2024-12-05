75e4755ff056ddee29877b1ebf2f074a90b318eb
curso15 <curso15@gmail.com>
2024-12-05 13:43:56 +0100

    realizado en forma parcial

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,17 +1,51 @@
-// Importar jQuery utilizando la sintaxis de módulos ES6
-import $ from 'jquery';
-
-// Esperamos que el DOM esté listo
 $(document).ready(function() {
-  // Asignar la acción de clic al botón
-  $('#boton').click(function() {
-    alert("¡Haz hecho clic en el botón!");
-  });
+    
+    solicitudes = [
+        solicitud = {
+            "id": 1,
+            "nombre": "Juan",
+            "apellido": "Secreto"
+        },
+        solicitud = {
+            "id": 1,
+            "nombre": "Alvaro",
+            "apellido": "Rueda"
+        },
+        solicitud = {
+            "id": 1,
+            "nombre": "Alfredo",
+            "apellido": "Galvan"
+        }];
+    
+
+    for(i=0; i < solicitudes.length; i++){
+        $("#maestro").append(
+            $("<li>").text(solicitudes[i].nombre + ' ' + solicitudes[i].apellido)
+        );
+    }
+
+    
+    $("#id").val(solicitud.id); // Fijaria el valor
+    $('#nombre').val(solicitud.nombre);
+    $('#apellido').val(solicitud.apellido);
+
+    $("li").on("click", function() {
+        if($("#detalle").is(":visible")){
+            $("#detalle").hide(); 
+        } else{ 
+            $("#detalle").hide();
+        }
+        
+    })
+
 });
 
 
-document.getElementById('boton').onclick = function() {
+/*document.getElementById('boton').onclick = function() {
     alert('Me ha presionado... me voy de vacaciones a relajarme');
 }
+document.getElementById('boton').onclick = function() {
+    this.innerText = 'Ya me ha comido <br> En otra linea';
+}
 
-console.log('Hola mundo... tercera vez');
+console.log('Hola mundo... tercera vez');*/
