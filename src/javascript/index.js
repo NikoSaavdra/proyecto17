57eb030bc01f4f2d258df6219b0b5c37bc0c9640
nicolas <nico@gmail.com>
2024-12-09 20:22:03 +0100

    botones terminados

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -7,8 +7,48 @@ $(function() {
     })
 
     $("#boton2").on("click", function(){
-        $.post("https://my-json-server.typicode.com/desarrollo-seguro/dato/solicitudes", function(data){
-            $("#resListar").text("Ok");
+        $.get("https://my-json-server.typicode.com/desarrollo-seguro/dato/solicitudes/1", function(data){
+            $("#resLeer").text("Ok");
+        })
+    });
+
+    $("#boton3").on("click", function(){
+        var datos = {
+              nombre: "Roberto",
+              apellido: "Carlos"
+        }
+        $.post("https://my-json-server.typicode.com/desarrollo-seguro/dato/solicitudes", datos, function(data){
+            $("#resCrear").text("Ok");
         })     
     })
+
+    $("#boton4").on("click", function(){
+        var actualizar = {
+            nombre: "Fernando Ariel",
+            apellido: "Dominguez"
+        };
+        $.ajax({
+            url: "https://my-json-server.typicode.com/desarrollo-seguro/dato/solicitudes/1",
+            type: "PUT", 
+            contentType: "application/json",
+            data: JSON.stringify(actualizar),
+            success: function(response) {
+                $("#resActualizar").text("Ok");
+                },   
+            });  
+    })
+
+    $("#boton5").on("click", function() {
+        $.ajax({
+            url: "https://my-json-server.typicode.com/desarrollo-seguro/dato/solicitudes/2",
+            type: "DELETE", 
+            success: function(response) {
+                $("#resBorrar").text("Ok");
+            },
+        });
+    });
 });
+
+
+
+
